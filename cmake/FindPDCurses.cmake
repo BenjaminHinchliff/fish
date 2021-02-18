d6be211a0a790726d2d8589310f7ec03de053be0
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2021-02-18 11:18:16 -0800

    split logic to get pdcurses into separate file

--- cmake/FindPDCurses.cmake
+++ cmake/FindPDCurses.cmake
@@ -10,13 +10,14 @@ FetchContent_Declare(
 FetchContent_GetProperties(pdcurses)
 if(NOT pdcurses_POPULATED)
 	FetchContent_Populate(pdcurses)
+	set(PDCURSES_LIB "${pdcurses_SOURCE_DIR}/wincon/pdcurses.lib")
 	add_custom_command(
-		OUTPUT "${pdcurses_SOURCE_DIR}/wincon/pdcurses.lib"
+		OUTPUT ${PDCURSES_LIB}
 		COMMAND nmake /f Makefile.vc
 		WORKING_DIRECTORY "${pdcurses_SOURCE_DIR}/wincon"
 	)
-	add_custom_target(pdcurses_build DEPENDS "${pdcurses_SOURCE_DIR}/wincon/pdcurses.lib")
+	add_custom_target(pdcurses_build DEPENDS ${PDCURSES_LIB})
 	add_library(pdcurses STATIC IMPORTED)
-	target_include_directories(pdcurses INTERFACE "${pdcurses_SOURCE_DIR}")
-	set_property(TARGET pdcurses PROPERTY IMPORTED_LOCATION "${pdcurses_SOURCE_DIR}/wincon/pdcurses.lib")
+	target_include_directories(pdcurses INTERFACE ${pdcurses_SOURCE_DIR})
+	set_property(TARGET pdcurses PROPERTY IMPORTED_LOCATION ${PDCURSES_LIB})
 endif()
