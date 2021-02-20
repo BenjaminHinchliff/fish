d33e78ca4c07e663702cb2e8c50252a48dcdb40e
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2021-02-19 16:44:46 -0800

    add buffering to prevent pdcurses crash

--- src/main.cpp
+++ src/main.cpp
@@ -56,10 +56,16 @@ void renderStacks(const Fish::stacks_t &stacks) {
 void renderDebugger(const Fish &fish, std::stringstream &output) {
   move(0, 0);
   renderGrid(fish);
+  refresh();
   renderStacks(fish.getStacks());
+  refresh();
   std::string outputString = output.str();
   printw("Output:\n");
-  printw("%s\n", outputString.c_str());
+  constexpr size_t seg_size = 500;
+  for (size_t i = 0; i < outputString.length() / seg_size + 1; ++i) {
+    printw(outputString.substr(i * seg_size, seg_size).c_str());
+  }
+  addch('\n');
   refresh();
 }
 
